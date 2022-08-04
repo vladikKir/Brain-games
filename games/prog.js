64fbf244a42922f8a0366be6bd9eded63deecdc0
Владислав Кирилюк <vladik15041999@gmail.com>
2022-08-04 03:27:51 +0300

    Add fixes in games/prog.js

--- games/prog.js
+++ games/prog.js
@@ -6,22 +6,23 @@ const expressions = [];
 const correctAnswers = [];
 
 for (let i = 0; i < 3; i += 1) {
-  const progressionLength = Math.round((Math.random() + 1) * 5);
+  const progressionLength = () => Math.round((Math.random() + 1) * 5);
   const progressionFirstNumber = Math.round(Math.random() * 10);
-  const progressionStep = Math.ceil(Math.random() * 7);
+  const progressionStep = Math.ceil((Math.random() + Math.random()) * 7);
 
   let progressionNumber = progressionFirstNumber;
-  let preogressionArray = '';
+  let preogressionArray = [];
 
-  for (let i2 = 1; i <= progressionLength; i2 += 1) {
+  for (let i = 1; i <= progressionLength; i += 1) {
     progressionNumber += progressionStep;
-    preogressionArray += ` ${progressionNumber}`;
+    preogressionArray.push(progressionNumber);
   }
   const numberHidden = Math.floor(Math.random() * progressionLength - 0.1);
   correctAnswers.push(preogressionArray[numberHidden]);
   preogressionArray[numberHidden] = '..';
-  preogressionArray = preogressionArray.join(' ')
+  preogressionArray = preogressionArray.join(' ');
   expressions.push(preogressionArray);
 }
+
 
 export default () => gameEngine(greeting, expressions, correctAnswers);
